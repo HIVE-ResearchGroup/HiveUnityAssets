cdc6a93e194ff26bcb1c22b915447726364feb4d
List Dominik <dominik.list95@gmail.com>
2022-05-03 11:13:37 +0200

    reverting and adding logger

---
+++ HiveUnityAssets/Packages/at.hive.utility/Runtime/LogTrackingData.cs
@@ -0,0 +1,73 @@
+﻿using Assets.Code.UserStudy;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class LogTrackingData : MonoBehaviour
+{
+
+    public bool doRecord = false;
+    public const float logInterval = 0.1f;
+    float logTimer = logInterval;
+
+    public StringBuilder sb;
+    public string filePath;
+
+    public int TaskNo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InitLogging(int personID)
+    {
+        logTimer = logInterval;
+        filePath = TrackingDataLogger.retrieveFilePath(this.gameObject, personID);
+        sb = new StringBuilder();
+    }
+
+    public void StartLogging(int taskNo)
+    {
+        doRecord = true;
+        TaskNo = taskNo;
+    }
+
+
+    public void StopLogging()
+    {
+        doRecord = false;
+        logTimer = logInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (doRecord)
+        {
+            logTimer -= Time.deltaTime;
+            if (logTimer<=0)
+            {
+                logTimer = logInterval;
+
+                float[] output = TrackingDataLogger.retrieveCommonTrackingData(TaskNo, this.gameObject);
+                sb = new StringBuilder();
+                sb = TrackingDataLogger.AddDataToLine(sb, output);
+
+                if (!File.Exists(filePath))
+                {
+                    File.WriteAllText(filePath, sb.ToString());
+                }
+                else
+                {
+                    File.AppendAllText(filePath, sb.ToString());
+                }
+
+            }
+        }
+    }
+}
---
+++ HiveUnityAssets/Packages/at.hive.utility/Runtime/TrackingDataLogger.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Code.UserStudy
+{
+    public static class TrackingDataLogger
+    {
+        public static string filePathPrefix = "C:\\_tracking_data\\";
+        public static string delimiter = ";";
+
+
+        public static string retrieveFilePath(GameObject o, int personID)
+        {
+            if (!Directory.Exists(filePathPrefix + DateTime.Now.ToString("yyyy-MM-dd")))
+            {
+                Directory.CreateDirectory(filePathPrefix + DateTime.Now.ToString("yyyy-MM-dd"));
+            }
+
+            string filePath = filePathPrefix + DateTime.Now.ToString("yyyy-MM-dd") + "\\" +
+                              personID +  "_" + o.name + "_" + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".txt";
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            return filePath;
+
+        }
+
+        public static float[] retrieveCommonTrackingData(int taskNo, GameObject o)
+        {
+            Vector3 trackerPosition = o.transform.position;
+            Vector3 trackerLocalPosition = o.transform.localPosition;
+            Vector3 trackerOrientation = o.transform.rotation.eulerAngles;
+            Quaternion trackerLocalOrientation = o.transform.localRotation;
+
+            float[] output = new float[]
+            {
+                taskNo,
+                trackerPosition.x,
+                trackerPosition.y,
+                trackerPosition.z,
+                /*trackerLocalPosition.x,
+                trackerLocalPosition.y,
+                trackerLocalPosition.z,*/
+                trackerOrientation.x,
+                trackerOrientation.y,
+                trackerOrientation.z
+                /*trackerLocalOrientation.w,
+                trackerLocalOrientation.x,
+                trackerLocalOrientation.y,
+                trackerLocalOrientation.z*/
+            };
+
+            return output;
+
+        }
+
+        public static StringBuilder AddDataToLine(StringBuilder sb, float[] trackingData)
+        {
+            int length = trackingData.Length;
+
+            if (sb == null)
+            {
+                sb = new StringBuilder();
+            }
+
+            for (int index = 0; index < length; index++)
+            {
+                sb.Append(trackingData[index] + delimiter);
+            }
+
+            sb.AppendLine();
+
+            return sb;
+
+        }
+
+    }
+}
